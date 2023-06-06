a46d93cdbeec657f4df8edf3130c6c0c2590580a
MatheusPTorquato <dev.matheusptorquato@gmail.com>
2023-06-06 20:48:30 -0300

    Removed

--- projeto180423_1/README.md
+++
@@ -1,4 +0,0 @@
-# Projeto 01 da aula do dia 18/abr/2023
-
-![Imagem do site](../imgs_projeto/projeto1_180423_1.png)
-![Imagem do site](../imgs_projeto/projeto1_180423_2.png)
--- projeto180423_1/src/componentes/Titulos/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import "./Style.css";
-
-export default function Titulos( props ) {
-  return(
-    <p className="pTitulo">{props.title}</p>
-  );
-}
